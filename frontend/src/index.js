0989fbab757c313b1eadc99f9af21494f856d43f
catherinekimani <catherinekimani882@gmail.com>
2025-07-25 11:39:53 +0300

    chore: move component styles to separate CSS files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
-
+import "./styles/global.css";
+import "./styles/components/Auth.css";
+import "./styles/components/Header.css";
+import "./styles/components/Dashboard.css";
+import "./styles/components/Category.css";
+import "./styles/components/Modal.css";
+import "./styles/utils/loading.css";
+import "./styles/utils/responsive.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
